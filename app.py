04e0f3d0b484c6c3472a73db49bb456f5303d24d
Francisca Barrera Berríos <franis@MacBook-Air-de-Francisca.local>
2026-07-16 20:37:37 -0400

    Solucionar puerto dinamico en app.py para render

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 application = get_wsgi_application()
 
 def main():
-    # Esto le dice a Python dónde están las configuraciones de tu sitio
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
     try:
         from django.core.management import execute_from_command_line
@@ -17,6 +16,16 @@ def main():
         raise ImportError(
             "No se pudo importar Django. ¿Está instalado?"
         ) from exc
+    
+    # 💡 DETECTAMOS EL PUERTO DE RENDER
+    # Si Render nos da un puerto, lo usamos. Si no (como en tu Mac), usamos el puerto 8000 por defecto.
+    port = os.environ.get("PORT", "8000")
+    
+    # Si ejecutas "python app.py" sin argumentos extras (como pasa en Render):
+    if len(sys.argv) == 1:
+        # Le decimos a Django que levante el servidor automáticamente en el puerto correcto
+        sys.argv = [sys.argv[0], "runserver", f"0.0.0.0:{port}"]
+        
     execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
